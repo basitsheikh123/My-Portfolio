ad7f57c9969cc55e978c96283dff9ef3b2cd562f
basitsheikh123 <mrbasit7777@gmail.com>
2025-10-19 17:10:11 +0500

    Fix base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ yahan apna GitHub repo name likho exactly jaisa GitHub par hai (case sensitive)
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio/',
+  base: './',   // <-- Ye line important hai
 })
